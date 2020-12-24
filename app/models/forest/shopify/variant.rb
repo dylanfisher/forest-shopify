16c94b7f05247c0a00b4d99c020265b0627c5281
dylanfisher <hi@dylanfisher.com>
2020-12-24 13:35:00 -0500

    Destroy dependent variant images on destroy

--- app/models/forest/shopify/variant.rb
+++ app/models/forest/shopify/variant.rb
@@ -5,7 +5,7 @@ module Forest::Shopify
 
     belongs_to :product, class_name: 'Forest::Shopify::Product', foreign_key: 'forest_shopify_product_id', optional: true
 
-    has_one :image, -> { order(id: :asc) }, as: :forest_shopify_record
+    has_one :image, -> { order(id: :asc) }, as: :forest_shopify_record, dependent: :destroy
     has_one :media_item, through: :image, source: :media_item
 
     def self.resource_description
