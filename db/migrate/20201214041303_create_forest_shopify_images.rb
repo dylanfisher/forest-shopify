fb12c546a58a85a8b5cdb2d19900ae74fce409db
dylanfisher <hi@dylanfisher.com>
2020-12-27 20:18:29 -0500

    Fix index name length in shopify images migration

--- db/migrate/20201214041303_create_forest_shopify_images.rb
+++ db/migrate/20201214041303_create_forest_shopify_images.rb
@@ -1,7 +1,7 @@
 class CreateForestShopifyImages < ActiveRecord::Migration[6.0]
   def change
     create_table :forest_shopify_images do |t|
-      t.references :forest_shopify_record, polymorphic: true
+      t.references :forest_shopify_record, polymorphic: true, index: { name: 'index_forest_shopify_imgs_on_f_s_record_type_and_f_s_record_id' }
       t.references :media_item, null: false, foreign_key: true
       t.string :shopify_id_base64
       t.text :alt_text
