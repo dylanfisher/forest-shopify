b8792fdc164399c1bd4fe6ca55f9d4d719b6b367
dylanfisher <hi@dylanfisher.com>
2021-03-25 16:50:14 -0400

    Touch associated records when updating a shopify image

--- app/models/forest/shopify/image.rb
+++ app/models/forest/shopify/image.rb
@@ -1,6 +1,6 @@
 module Forest::Shopify
   class Image < Forest::Shopify::ApplicationRecord
-    belongs_to :forest_shopify_record, polymorphic: true
-    belongs_to :media_item, dependent: :destroy
+    belongs_to :forest_shopify_record, polymorphic: true, touch: true
+    belongs_to :media_item, dependent: :destroy, touch: true
   end
 end
